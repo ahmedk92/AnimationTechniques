f140b1a93ef5dcfa1baec0f8910af80dbe812d04
Ahmed Khalaf <ahmedkhalaf.92@gmail.com>
2020-01-10 22:54:29 +0200

    Initial Commit

---
+++ AnimationTechniques/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnimationTechniques
+//
+//  Created by Ahmed Khalaf on 1/10/20.
+//  Copyright © 2020 com.ahmedkhalaf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
